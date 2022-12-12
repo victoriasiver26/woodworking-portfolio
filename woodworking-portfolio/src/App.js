32b2cd6c2f4f370e00168f460ed4aac4010bbdee
Victoria Siver <vsiver@ufl.edu>
2022-12-12 02:29:34 -0500

    created react app installed bootstrap, added template navbar

--- woodworking-portfolio/src/App.js
+++ woodworking-portfolio/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css"
 import {
+  Landing,
   Navbar,
+  Project1,
+  Project2,
 } from "./components";
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div class="page-container">
         <Navbar/>
-        <h1> Hello World!</h1>
-    </div>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/project1" element={<Project1 />} />
+          <Route path="/project2" element={<Project2 />} />
+          <Route path="*" element={<h1> ERROR 404: PAGE NOT FOUND </ h1>} />
+        </Routes>
+      </div>
+    </Router>
+   
   );
 }
 
